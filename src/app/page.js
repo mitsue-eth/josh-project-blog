613703ee544b21581ea0550f83d0ab61090d5e1f
Mitsue <mitsue.eth@gmail.com>
2023-10-19 16:25:19 +0000

    first excercise almost done

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,14 @@ import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
 import styles from './homepage.module.css';
+import { getBlogPostList } from '@/helpers/file-helpers';
 
 async function Home() {
-  
+
+  const blogPosts = await getBlogPostList();
+
+  console.log('Blogposts: ', blogPosts);
+
   const filesArray = fs.readdir('content', (err, files) => {
     if (err) throw err;
     console.log(files);
@@ -16,15 +21,21 @@ async function Home() {
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
-      
-      
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
-      
+
+      {blogPosts.map(post => {
+        const { slug, title, abstract, publishedOn } = post;
+        return (
+
+          <BlogSummaryCard
+            slug={slug}
+            title={title}
+            abstract={abstract}
+            publishedOn={publishedOn}
+          />
+        )
+      })}
+
+
     </div>
   );
 }
