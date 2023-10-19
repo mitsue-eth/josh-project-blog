41e9c3667ee0f51430511ed8e4e296c56d6fb6bb
Mitsue <mitsue.eth@gmail.com>
2023-10-19 07:28:07 +0000

    started working with repo and doing first task

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,30 @@
+import fs from 'fs';
 import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
-
 import styles from './homepage.module.css';
 
-function Home() {
+async function Home() {
+  
+  const filesArray = fs.readdir('content', (err, files) => {
+    if (err) throw err;
+    console.log(files);
+  });
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
+      
+      
       <BlogSummaryCard
         slug="example"
         title="Hello world!"
         abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
         publishedOn={new Date()}
       />
+      
     </div>
   );
 }
