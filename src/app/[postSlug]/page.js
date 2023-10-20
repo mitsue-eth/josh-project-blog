d59570d5faf56255a74bdfee9286e35a7125c9df
Mitsue <mitsue.eth@gmail.com>
2023-10-20 11:15:20 +0000

    finalised excercise 2

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -9,27 +9,30 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
 
 async function loadBlogPost(slug) {
-  const rawContent = await fs.readFile(`/content/${slug}.mdx`);
-  const { data: frontMatter, content } = rawContent;
+  const rawContent = await fs.readFile(
+    path.join(process.cwd(), `/content/${slug}.mdx`),
+    "utf8"
+  );
+  const { data: frontmatter, content } = matter(rawContent);
 
-  return { frontMatter, content };
+  return { frontmatter, content };
 }
 
 async function BlogPost({ params }) {
   const { postSlug } = params;
 
-  const { frontMatter, content } = loadBlogPost(params.postSlug);
-  console.log({ frontMatter });
+  console.log(params);
+
+  const { frontmatter, content } = await loadBlogPost(postSlug);
 
   return (
     <article className={styles.wrapper}>
-      <BlogHero title="Example post!" publishedOn={new Date()} />
+      <BlogHero
+        title={frontmatter.title}
+        publishedOn={new Date(frontmatter.publishedOn)}
+      />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of the elements
-          created from the blog post in this spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
