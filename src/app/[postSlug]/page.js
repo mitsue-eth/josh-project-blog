4d9f512c0f83681d6f5901c227512cd09246c677
Mitsue <mitsue.eth@gmail.com>
2023-10-20 04:50:19 +0000

    wip chapter 1

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
+import fs from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
 
-function BlogPost() {
+async function loadBlogPost(slug) {
+  const rawContent = await fs.readFile(`/content/${slug}.mdx`);
+  const { data: frontMatter, content } = rawContent;
+
+  return { frontMatter, content };
+}
+
+async function BlogPost({ params }) {
+  const { postSlug } = params;
+
+  const { frontMatter, content } = loadBlogPost(params.postSlug);
+  console.log({ frontMatter });
+
   return (
     <article className={styles.wrapper}>
-      <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
-      />
+      <BlogHero title="Example post!" publishedOn={new Date()} />
       <div className={styles.page}>
         <p>This is where the blog post will go!</p>
         <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
+          You will need to use <em>MDX</em> to render all of the elements
+          created from the blog post in this spot.
         </p>
       </div>
     </article>
